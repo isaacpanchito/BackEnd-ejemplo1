1a868ffd4356c83b47391031d9f1d7964123f4b9
isaacpanchito <isaacemj.26@gmail.com>
2022-10-24 18:01:30 -0500

    Agregar archivos de proyecto.

---
+++ BackEnd-ejemplo1/Controllers/CalculosBasController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BackEnd_ejemplo1.Models;
+
+namespace BackEnd_ejemplo1.Controllers
+{
+    public class CalculosBasController : Controller
+    {
+         //GET: CalculosBas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma(string Num1, string Num2)
+        {
+            int res = Convert.ToInt16(Num1) + Convert.ToInt16(Num2);
+            ViewBag.Res = Convert.ToString(res);
+            return View();
+        }
+
+        public ActionResult Operas()
+        {
+            OperaBas op = new OperaBas();
+            return View(op);
+        }
+        public ActionResult Operas(OperaBas op)
+        {
+            op.Suma();
+            ViewBag.Res = op.Res;
+            return View(op);
+        }
+        [HttpPost]
+        public ActionResult Resta(OperaBas op)
+        {
+            op.Resta();
+            return View(op);
+        }
+    }
+}
---
+++ BackEnd-ejemplo1/Services/PeliculaService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BackEnd_ejemplo1.Models;
+
+namespace BackEnd_ejemplo1.Services
+{
+    public class PeliculaService
+    {
+        public List<Pelicula> ObtenerPelicula()
+        {
+            var pelicula1 = new Models.Pelicula()
+            {
+                Titulo = "Superman",
+                Duracion = 150,
+                Pais = "USA",
+                Publicacion = new DateTime(2011, 12, 11)
+            };
+            var pelicula2 = new Models.Pelicula()
+            {
+                Titulo = "Spiderman",
+                Duracion = 180,
+                Pais = "USA",
+                Publicacion = new DateTime(2015, 05, 02)
+            };
+            return new List<Pelicula> { pelicula1,pelicula2};
+        }
+    }
+}
